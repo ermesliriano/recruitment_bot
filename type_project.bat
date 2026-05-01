29e20ed55f8ab142858b9b25b831e44cb0bfd593
Ermes Liriano <ermesliriano@mercedarios.net>
2026-05-01 22:35:43 +0200

    created .bat file for type all code and deploy files

--- type_project.bat
+++ type_project.bat
@@ -1,9 +1,26 @@
 @echo off
-:: Recorre de forma recursiva (/r) buscando archivos .py
-for /r %%f in (*.py) do (
-    echo.
-    echo --- Leyendo: %%f ---
-    type "%%f"
-    echo.
+setlocal enabledelayedexpansion
+
+:: Directorio actual
+set "target_dir=%~dp0"
+
+echo ====================================================
+echo Listando archivos: .py, .env, .txt, .yaml, .sql y Dockerfile
+echo Directorio: %target_dir%
+echo ====================================================
+
+:: Usamos un formato de cabecera que no use caracteres especiales de redireccion
+for /r "%target_dir%" %%f in (*.py .env *.txt *.yaml Dockerfile *.sql) do (
+    if exist "%%f" (
+        echo.
+        echo [ FICHERO: %%f ]
+        echo ----------------------------------------------------
+        type "%%f"
+        echo.
+        echo ----------------------------------------------------
+    )
 )
+
+echo.
+echo Proceso finalizado.
 pause
